7c219df29cb3e3880646f1e9220dd596c946ab47
Alvi Sait <alvijonathan.sait@gmail.com>
2016-08-23 11:18:13 +1000

    Updated navbar using Bulma CSS

--- src/js/components/layout/Nav.js
+++ src/js/components/layout/Nav.js
@@ -14,32 +14,79 @@ export default class Nav extends React.Component {
   }
 
   render() {
+    const { collapsed } = this.state;
+
+    const navClass = collapsed ? "" : "is-active";
+
     return (
-      <nav class="uk-navbar uk-margin-large-bottom">
-        <a class="uk-navbar-brand uk-hidden-small" href="layouts_frontpage.html">Brand</a>
-        <ul class="uk-navbar-nav uk-hidden-small">
-            <li>
-                <a href="layouts_frontpage.html">Frontpage</a>
-            </li>
-            <li class="uk-active">
-                <a href="layouts_portfolio.html">Portfolio</a>
-            </li>
-            <li>
-                <a href="layouts_blog.html">Blog</a>
-            </li>
-            <li>
-                <a href="layouts_documentation.html">Documentation</a>
-            </li>
-            <li>
-                <a href="layouts_contact.html">Contact</a>
-            </li>
-            <li>
-                <a href="layouts_login.html">Login</a>
-            </li>
-        </ul>
-        <a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas=""/>
-        <div class="uk-navbar-brand uk-navbar-center uk-visible-small">Brand</div>
-    </nav>
+      <section class="hero is-dark is-bold">
+        <div class="hero-head">
+          <div class="container">
+            <nav class="nav">
+              <div class="nav-left">
+                <a class="nav-item is-brand" href="/">
+                  movie404
+                </a>
+              </div>
+
+              <div class="nav-center">
+                <a class="nav-item" href="https://github.com/jgthms/bulma">
+                  <span class="icon">
+                    <i class="fa fa-github"/>
+                  </span>
+                </a>
+                <a class="nav-item" href="https://twitter.com/jgthms">
+                  <span class="icon">
+                    <i class="fa fa-twitter"/>
+                  </span>
+                </a>
+              </div>
+
+              <span id="nav-toggle" class={`nav-toggle ${navClass}`}
+                onClick={this.toggleCollapse.bind(this)}>
+                <span/>
+                <span/>
+                <span/>
+              </span>
+
+              <div id="nav-menu" class={`nav-right nav-menu ${navClass}`}>
+                <a class="nav-item is-active">
+                  Home
+                </a>
+                <a class="nav-item">
+                  Examples
+                </a>
+                <a class="nav-item">
+                  Documentation
+                </a>
+                <span class="nav-item">
+                  <a class="button is-dark is-inverted">
+                    <span class="icon">
+                      <i class="fa fa-github"/>
+                    </span>
+                    <span>Download</span>
+                  </a>
+                </span>
+              </div>
+            </nav>
+          </div>
+        </div>
+
+        <div class="hero-body">
+          <div class="container">
+            <div class="columns is-vcentered">
+              <div class="column">
+                <p class="title">
+                  Documentation
+                </p>
+                <p class="subtitle">
+                  Everything you need to <strong>create a website</strong> with Bulma
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
